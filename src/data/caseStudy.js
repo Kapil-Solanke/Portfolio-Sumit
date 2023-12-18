e0ff44175abd70ee7f00c3e3ace6ff48b1bc1308
sumitgaurav25 <119870937+sumitgaurav25@users.noreply.github.com>
2023-12-18 13:40:56 +0530

    Update caseStudy.js

--- src/data/caseStudy.js
+++ src/data/caseStudy.js
@@ -2,7 +2,7 @@ export const caseStudy = [
     {
         "t1": "CASE STUDY #1",
         "t2": "12 MAR 2023",
-        "head": "The impressive services",
+        "head": "Fasthetics - Fashion App Case Study",
         "para": "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum",
         "bottom": "READ FULL CASE STUDY"
     },
